29582ddbad32a30cd6baa3ae37d36a494a574d0b
naifibrahim <naifibrahim770@gmail.com>
2023-01-27 10:10:00 +0500

    JS

---
+++ app.js
@@ -0,0 +1,18 @@
+alert ("JavaScript Practice")
+
+var name = "Naif"
+var nationality = "Pakistani"
+var jd = "Sales Manager"
+
+alert (name)
+alert (nationality)
+alert (jd)
+
+var age = 19
+alert (age)
+
+var ageAddedBySomeone = 6
+alert (ageAddedBySomeone)
+
+var totalAge =  age + ageAddedBySomeone
+alert (totalAge)
