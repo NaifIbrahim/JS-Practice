7f1a0888bd11207a3b2b2672801b236cd41d1f0a
naifibrahim <naifibrahim770@gmail.com>
2023-02-01 09:42:42 +0500

    JS practice

--- app.js
+++ app.js
@@ -16,3 +16,20 @@ alert (ageAddedBySomeone)
 
 var totalAge =  age + ageAddedBySomeone
 alert (totalAge)
+
+// var a = 1;
+// alert (a)  //1
+
+// alert (a++)  //1
+// //2
+// alert(a)  //2
+
+// alert (++a)  // 3
+// alert(a)  // 3
+
+var firstName = prompt("Enter your first name")
+var lastName = prompt("Enter your last name")
+var age = prompt("Enter your age")
+var mobile_number = prompt("Enter your mobile number")
+
+alert (firstName + " " + lastName + " " + age + " " + mobile_number)
