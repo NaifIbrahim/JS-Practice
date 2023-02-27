d2c0347fa29025dc624075e8b12d7e725fdd0d9a
NaifIbrahim <naifibrahim770@gmail.com>
2023-02-27 10:37:53 +0500

    assignments

---
+++ Assignments/app4.js
@@ -0,0 +1,49 @@
+// QUESTION NO 6
+// var hour = prompt("ENTER TIME(HOUR) IN 24H FORMAT")
+
+// if (hour < 18){
+//  alert("GOOD DAY")
+// }
+
+// else{
+//     alert("GOOD EVENING")
+// }
+
+// QUESTION NO 5 
+// var userPwFromDB = "naif123"
+
+// var askingPw = prompt("enter your password")
+
+// if(askingPw == userPwFromDB){
+//     alert("you are going to signing in")
+// }
+
+// else if(askingPw == ""){
+//  alert("please enter your password")
+// }
+
+// else if (askingPw !== userPwFromDB){
+//  alert("password is incorrect")
+// }
+
+// QUESTION NO 4
+
+// var askingVowel = prompt ("write a alphabet")
+// if (askingVowel == "a"|| askingVowel == "e" || askingVowel == "i" || askingVowel == "o" || askingVowel == "u"){
+//     console.log(true)
+// }
+
+// else {
+//     console.log(false)
+// }
+
+// QUESTION NO 3
+
+// var askingValue = prompt("enter any number")
+// if (askingValue >0){
+//  console.log("Your Number Is Positive")
+// }
+
+// else{
+//     console.log("Your Number Is Negative")
+// }
