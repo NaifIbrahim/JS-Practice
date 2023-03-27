a536a69cc629a530e03a7f572f4aed0f90e3c972
NaifIbrahim <naifibrahim770@gmail.com>
2023-03-27 10:04:59 +0500

    forEach,find,filter work

--- DOM/map/app.js
+++ DOM/map/app.js
@@ -1,24 +1,52 @@
 // MAP USE
-const numbers = [65, 44, 12, 4];
-const newArr = numbers.map(myFunction)
+// const numbers = [65, 44, 12, 4];
+// const newArr = numbers.map(myFunction)
 
-function myFunction(num) {
-  return num * 10;
-}
-console.log(newArr)
+// function myFunction(num) {
+//   return num * 10;
+// }
+// console.log(newArr)
 
 // CALLBACK FUNCTION
+// const bakeryitems = ["jam","bun","eggs","milk","butter","biscuits"]
 
-const bakeryitems = ["jam","bun","eggs","milk","butter","biscuits"]
+// function myfunc(abc){
+//  console.log(bakeryitems)
+//  abc()
+// }
 
-function myfunc(abc){
- console.log(bakeryitems)
- abc()
-}
+// myfunc(myfunc2)
 
-myfunc(myfunc2)
+// function myfunc2(){
+//     console.log("Ye sab available hai bakery me")
+//    }
 
-function myfunc2(){
-    console.log("Ye sab available hai bakery me")
-   }
-   
+// FILTER
+// const ages = [32, 33, 16, 40];
+// const result = ages.filter(checkAdult);
+
+// function checkAdult(age) {
+//   return age >= 18;
+// }
+
+// console.log(result)
+
+// ForEach
+// let text = "";
+// const fruits = ["apple", "orange", "cherry"];
+// fruits.forEach(myFunction);
+
+// document.getElementById("demo").innerHTML = text;
+ 
+// function myFunction(item, index) {
+//   text += index + ": " + item + "<br>"; 
+// }
+
+// Find
+// const ages = [3, 10, 18, 20];
+
+// document.getElementById("demo").innerHTML = ages.find(checkAge);
+
+// function checkAge(age) {
+//   return age >18;
+// }
